2d2ef26a6ebf109ebaee2aac918bd2d78e78b2e9
Larissa <trueblar@students.zhaw.ch>
2026-05-06 10:09:58 +0200

    home angepasst

--- views/home.py
+++ views/home.py
@@ -1,8 +1,17 @@
 import streamlit as st
 
+st.title("ReadMe, {benutzername}!")
+
+st.subheader("Willkommen!")
+# Hier würde der Benutzername vom Profil eingefügt werden
+# st.write(f"Willkommen, {benutzername}!")
+
+st.write("""
+Diese App ermöglicht Lehrpersonen, Texte einfach und strukturiert bereitzustellen. Schülerinnen und Schüler können die veröffentlichten Inhalte lesen und ihr Verständnis anschliessend durch ein integriertes Quiz überprüfen.
+""")
+
+
 st.title("Meine erste Streamlit App")
-
-
 
 """
 Diese App richtet sich an Lehrpersonen, die den fortschritt ihrer Studierenden überprüfen möchten, ind dem Sie überprüfen können ob die augegebenen dokumente gelesen wurden und bestanden wurden.
